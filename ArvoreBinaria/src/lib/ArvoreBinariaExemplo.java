b73a98d978d90036527028e01136b93e33e115b7
ericklyl <109321308+ericklyl@users.noreply.github.com>
2024-04-03 21:18:18 -0300

    Add files via upload

---
+++ ArvoreBinaria/src/lib/ArvoreBinariaExemplo.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+
+/**
+ *
+ * @author Erick Loyola
+ */
+public class ArvoreBinariaExemplo<T> implements IArvoreBinaria<T> {
+    
+    protected NoExemplo<T> raiz = null;
+    protected Comparator<T> comparador;
+
+    protected NoExemplo<T> atual = null;
+    private Stack<NoExemplo<T>> pilha = new Stack<>();
+  
+    public ArvoreBinariaExemplo(Comparator<T> comp) {
+
+        comparador = comp;
+    }
+    
+    @Override
+    public void adicionar(T novoValor) {
+        raiz = adicionarRecusivamente(raiz,novoValor);
+    }
+
+    @Override
+    public T pesquisar(T valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   @Override
+    public T pesquisar(T valor, Comparator comparador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T remover(T valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int altura() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+       
+    
+    @Override
+    public int quantidadeNos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String caminharEmNivel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.    
+    }
+    
+    @Override
+    public String caminharEmOrdem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.    
+    }
+        
+}
